587a457debff484a0de1f5fee7b0a4fccee67ab7
Oliver Binns <mail@oliverbinns.co.uk>
2021-11-11 09:05:30 +0000

    Check if branch is triggered

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo $CI_WORKFLOW;
 if [ CI_WORKFLOW = 'Pull Request Validation' ]
 then
+    echo $CI_WORKFLOW;
 	./run_swiftlint.sh
-        ./install_sonar_dependencies.sh
+    ./install_sonar_dependencies.sh
 fi
