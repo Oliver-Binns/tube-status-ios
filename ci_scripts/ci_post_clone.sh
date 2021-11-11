ad27f4c6d56ec0aa78d717cb3909ddf1e640d558
Oliver Binns <mail@oliverbinns.co.uk>
2021-11-11 09:11:41 +0000

    Added else branch

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,7 +2,11 @@
 
 if [ CI_WORKFLOW = 'Pull Request Validation' ]
 then
-    echo $CI_WORKFLOW;
-	./run_swiftlint.sh
-    ./install_sonar_dependencies.sh
+  echo $CI_WORKFLOW;
+  ./run_swiftlint.sh
+  ./install_sonar_dependencies.sh
+else
+  echo $CI_WORKSPACE;
+  ./run_swiftlint.sh
+  ./install_sonar_dependencies.sh
 fi
