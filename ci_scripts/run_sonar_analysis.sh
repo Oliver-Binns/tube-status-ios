d92cf7bae2d0cc92c74351f54aba895816022302
Oliver Binns <mail@oliverbinns.co.uk>
2021-11-22 09:02:36 +0000

    Added branch names, etc. to SonarAnalysis

--- ci_scripts/run_sonar_analysis.sh
+++ ci_scripts/run_sonar_analysis.sh
@@ -14,8 +14,12 @@ fastlane run sonar \
   project_name:"tube-status-ios" \
   project_version:"1.0" \
   project_language:"swift" \
-  sonar_runner_args:"-Dsonar.c.file.suffixes=- -Dsonar.cpp.file.suffixes=- -Dsonar.objc.file.suffixes=-" \
+  sonar_runner_args:"-Dsonar.projectBaseDir=$CI_WORKSPACE -Dsonar.c.file.suffixes=- -Dsonar.cpp.file.suffixes=- -Dsonar.objc.file.suffixes=-" \
   sources_path:$CI_WORKSPACE \
   sonar_organization:"oliver-binns" \
   sonar_login:$SONAR_TOKEN \
-  sonar_url:"https://sonarcloud.io"
+  sonar_url:"https://sonarcloud.io" \
+  pull_request_branch: $CI_PULL_REQUEST_SOURCE_BRANCH \
+  pull_request_base: $CI_PULL_REQUEST_TARGET_BRANCH \
+  pull_request_key: $CI_PULL_REQUEST_NUMBER
+
