b99998de83c26f69cf3e0684aa474d1cd51afc02
Oliver Binns <mail@oliverbinns.co.uk>
2021-11-22 08:23:43 +0000

    Using Fastlane action for Sonar

--- ci_scripts/install_sonar_dependencies.sh
+++ ci_scripts/install_sonar_dependencies.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-export SONAR_SCANNER_VERSION=4.4.0.2170                      
-export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-macosx
-curl --create-dirs -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-macosx.zip
-unzip -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
-export PATH=$SONAR_SCANNER_HOME/bin:$PATH
-export SONAR_SCANNER_OPTS="-server"
+brew install sonar-scanner
--- ci_scripts/run_sonar_analysis.sh
+++ ci_scripts/run_sonar_analysis.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
-sonar-scanner \
-  -Dsonar.organization=oliver-binns \
-  -Dsonar.projectKey=tube-status-ios \
-  -Dsonar.sources=. \
-  -Dsonar.host.url=https://sonarcloud.io \
-  -Dsonar.c.file.suffixes=- \
-  -Dsonar.cpp.file.suffixes=- \
-  -Dsonar.objc.file.suffixes=-
+#sonar-scanner \
+#  -Dsonar.organization=oliver-binns \
+#  -Dsonar.projectKey=tube-status-ios \
+#  -Dsonar.sources=. \
+#  -Dsonar.host.url=https://sonarcloud.io \
+#  -Dsonar.c.file.suffixes=- \
+#  -Dsonar.cpp.file.suffixes=- \
+#  -Dsonar.objc.file.suffixes=-
+
+fastlane run sonar \
+  project_key:"tube-status-ios" \
+  project_name:"tube-status-ios" \
+  project_version:"1.0" \
+  project_language:"swift" \
+  sonar_runner_args:"-Dsonar.c.file.suffixes=- -Dsonar.cpp.file.suffixes=- -Dsonar.objc.file.suffixes=-" \
+  sources_path:$CI_WORKSPACE \
+  sonar_organization:"oliver-binns" \
+  sonar_login:$SONAR_TOKEN \
+  sonar_url:"https://sonarcloud.io"
