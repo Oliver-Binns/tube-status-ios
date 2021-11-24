a6a843e556030de8f1a1eb7f8e39977bf89d449c
Oliver Binns <mail@oliverbinns.co.uk>
2021-11-24 22:08:30 +0000

    tweak

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,7 +5,7 @@ if [ "$CI_WORKFLOW" = "Pull Request Validation" ]
 then
   git checkout -b temp
   git branch -d develop
-  git fetch origin develop
+  git fetch origin
   git checkout develop
   git checkout temp
 
