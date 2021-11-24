d566da0b55b6cff6c5c0d2f32406ca8cb6975f1f
Oliver Binns <mail@oliverbinns.co.uk>
2021-11-24 21:22:58 +0000

    Remove Homebrew autoupdate, re-fetch develop branch

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,5 +5,6 @@ if [ "$CI_WORKFLOW" = "Pull Request Validation" ]
 then
   git checkout -b temp
   git branch -d develop
+  git fetch origin develop
   ./install_dependencies.sh
 fi
--- ci_scripts/install_dependencies.sh
+++ ci_scripts/install_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+export HOMEBREW_NO_AUTO_UPDATE=1
 brew install fastlane
 
 if [ "$CI_WORKFLOW" = "Pull Request Validation" ]
