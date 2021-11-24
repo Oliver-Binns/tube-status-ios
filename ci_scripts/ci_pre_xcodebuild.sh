222f95d0eba310197fa4291638fa9de2551b3771
Oliver Binns <mail@oliverbinns.co.uk>
2021-11-24 09:05:11 +0000

    Added badge to App Icon

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -3,8 +3,8 @@ set -e
 
 if [ "$CI_WORKFLOW" = "Alpha Release" ]
 then
-    fastlane run badge alpha:true
+    fastlane run badge alpha:true path:$CI_WORKSPACE
 elif [ "$CI_WORKFLOW" = "Beta Release" ]
 then
-    fastlane run badge dark:true
+    fastlane run badge dark:true path:$CI_WORKSPACE
 fi
