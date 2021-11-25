a0cef150b0c7728c908da66b95e0f3132792d03c
Oliver Binns <mail@oliverbinns.co.uk>
2021-11-25 07:31:48 +0000

    Reset develop to remote

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,10 +3,8 @@ set -e
 
 if [ "$CI_WORKFLOW" = "Pull Request Validation" ]
 then
-  git -C $CI_WORKSPACE checkout -b temp
-  git -C $CI_WORKSPACE branch -d develop
-  git -C $CI_WORKSPACE pull
-  git -C $CI_WORKSPACE checkout develop
+  git -C $CI_WORKSPACE branch temp
+  git -C $CI_WORKSPACE reset --hard origin/develop
   git -C $CI_WORKSPACE checkout temp
 
   ./install_dependencies.sh
