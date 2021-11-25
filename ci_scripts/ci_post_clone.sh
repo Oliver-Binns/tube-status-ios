04d92fa342e1ae4ad977b63cf9cd4ebde3312a60
Oliver BINNS <mail@oliverbinns.co.uk>
2021-11-25 09:14:42 +0000

    Avoid swapping branches

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,6 +3,7 @@ set -e
 
 if [ "$CI_WORKFLOW" = "Pull Request Validation" ]
 then
+  ./install_dependencies.sh
   # fetch a reference to the develop branch on GitHub
   # this will allow SonarQube analysis to work
   git -C $CI_WORKSPACE checkout -b temp
@@ -14,11 +15,9 @@ then
     "+refs/heads/$CI_PULL_REQUEST_TARGET_BRANCH:refs/remotes/origin/$CI_PULL_REQUEST_TARGET_BRANCH"
   git -C $CI_WORKSPACE fetch
 
-  git -C $CI_WORKSPACE checkout develop
-  git -C $CI_WORKSPACE log -n 2
+  #git -C $CI_WORKSPACE checkout develop
+  #git -C $CI_WORKSPACE log -n 2
 
-  git -C $CI_WORKSPACE checkout temp
-  git -C $CI_WORKSPACE log -n 2
-
-  ./install_dependencies.sh
+  #git -C $CI_WORKSPACE checkout temp
+  #git -C $CI_WORKSPACE log -n 2
 fi
