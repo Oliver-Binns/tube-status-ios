5f532afeb5faafb8dca9aac90c75c1fe77ce70b0
Oliver BINNS <mail@oliverbinns.co.uk>
2021-11-25 08:10:01 +0000

    fix refspec

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,7 +5,7 @@ if [ "$CI_WORKFLOW" = "Pull Request Validation" ]
 then
   git -C $CI_WORKSPACE branch -r
   git -C $CI_WORKSPACE branch temp
-  git -C $CI_WORKSPACE config --replace-all remote.origin.fetch '+refs/develop:refs/remotes/origin/develop'
+  git -C $CI_WORKSPACE config --replace-all remote.origin.fetch '+refs/heads/develop:refs/remotes/origin/develop'
   git -C $CI_WORKSPACE fetch
   git -C $CI_WORKSPACE branch -r
   git -C $CI_WORKSPACE reset --hard origin/develop
