2454d222f9f1738907f918eba31274af458b7049
Oliver BINNS <mail@oliverbinns.co.uk>
2021-11-25 08:06:21 +0000

    Added a git fetch

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,6 +6,7 @@ then
   git -C $CI_WORKSPACE branch -r
   git -C $CI_WORKSPACE branch temp
   git -C $CI_WORKSPACE config --replace-all remote.origin.fetch '+refs/develop:refs/remotes/origin/develop'
+  git -C $CI_WORKSPACE fetch
   git -C $CI_WORKSPACE branch -r
   git -C $CI_WORKSPACE reset --hard origin/develop
   git -C $CI_WORKSPACE checkout temp
