d890a0bd864ce7c2158ee7e3afe0a9658847ab48
Oliver Binns <mail@oliverbinns.co.uk>
2021-11-25 07:27:27 +0000

    Added path to git commands

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,11 +3,11 @@ set -e
 
 if [ "$CI_WORKFLOW" = "Pull Request Validation" ]
 then
-  git checkout -b temp
-  git branch -d develop
-  git pull origin develop
-  git checkout develop
-  git checkout temp
+  git -C $CI_WORKSPACE checkout -b temp
+  git -C $CI_WORKSPACE branch -d develop
+  git -C $CI_WORKSPACE pull
+  git -C $CI_WORKSPACE checkout develop
+  git -C $CI_WORKSPACE checkout temp
 
   ./install_dependencies.sh
 fi
