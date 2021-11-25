ffeff2e3bc9fd2e25341545b500149d9841ccc54
Oliver BINNS <mail@oliverbinns.co.uk>
2021-11-25 08:14:25 +0000

    Removed checking out branches

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,13 +3,10 @@ set -e
 
 if [ "$CI_WORKFLOW" = "Pull Request Validation" ]
 then
-  git -C $CI_WORKSPACE branch -r
-  git -C $CI_WORKSPACE branch temp
+  #git -C $CI_WORKSPACE branch temp
   git -C $CI_WORKSPACE config --replace-all remote.origin.fetch '+refs/heads/develop:refs/remotes/origin/develop'
   git -C $CI_WORKSPACE fetch
-  git -C $CI_WORKSPACE branch -r
-  git -C $CI_WORKSPACE reset --hard origin/develop
-  git -C $CI_WORKSPACE checkout temp
-
+  #git -C $CI_WORKSPACE reset --hard origin/develop
+  #git -C $CI_WORKSPACE checkout temp
   ./install_dependencies.sh
 fi
