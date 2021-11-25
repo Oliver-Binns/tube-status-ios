84ead9a4dbb92edd7b9d3733a9735d8821c9558d
Oliver BINNS <mail@oliverbinns.co.uk>
2021-11-25 09:06:55 +0000

    Create a reference to the develop branch

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,8 +5,19 @@ if [ "$CI_WORKFLOW" = "Pull Request Validation" ]
 then
   # fetch a reference to the develop branch on GitHub
   # this will allow SonarQube analysis to work
-  git -C $CI_WORKSPACE config --replace-all remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
+  git -C $CI_WORKSPACE branch temp
+
+  git -C $CI_WORKSPACE config remote.origin.fetch \
+    "+refs/heads/$CI_PULL_REQUEST_SOURCE_BRANCH:refs/remotes/origin/$CI_PULL_REQUEST_SOURCE_BRANCH"
+  git -C $CI_WORKSPACE config remote.origin.fetch \
+    "+refs/heads/$CI_PULL_REQUEST_TARGET_BRANCH:refs/remotes/origin/$CI_PULL_REQUEST_TARGET_BRANCH"
   git -C $CI_WORKSPACE fetch
+
+  git -C $CI_WORKSPACE checkout develop
+  git -C $CI_WORKSPACE log -n 2
+
+  git -C $CI_WORKSPACE checkout temp
+  git -C $CI_WORKSPACE log -n 2
 
   ./install_dependencies.sh
 fi
