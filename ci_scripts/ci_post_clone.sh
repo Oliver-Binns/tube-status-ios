ea638276794b2ecd2cef79bcd46eeaf88eea2a5b
Oliver BINNS <mail@oliverbinns.co.uk>
2021-11-25 09:11:26 +0000

    Clear develop branch

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,7 +5,8 @@ if [ "$CI_WORKFLOW" = "Pull Request Validation" ]
 then
   # fetch a reference to the develop branch on GitHub
   # this will allow SonarQube analysis to work
-  git -C $CI_WORKSPACE branch temp
+  git -C $CI_WORKSPACE checkout -b temp
+  git -C $CI_WORKSPACE branch -d develop
 
   git -C $CI_WORKSPACE config remote.origin.fetch \
     "+refs/heads/$CI_PULL_REQUEST_SOURCE_BRANCH:refs/remotes/origin/$CI_PULL_REQUEST_SOURCE_BRANCH"
