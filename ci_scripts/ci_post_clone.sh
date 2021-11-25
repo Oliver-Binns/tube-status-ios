b24974d251741c4df76b28bb9652e7a961d301ef
Oliver BINNS <mail@oliverbinns.co.uk>
2021-11-25 07:51:16 +0000

    List out remotes and branches

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,6 +3,8 @@ set -e
 
 if [ "$CI_WORKFLOW" = "Pull Request Validation" ]
 then
+  git -C $CI_WORKSPACE remote
+  git -C $CI_WORKSPACE branch
   git -C $CI_WORKSPACE branch temp
   git -C $CI_WORKSPACE reset --hard origin/develop
   git -C $CI_WORKSPACE checkout temp
