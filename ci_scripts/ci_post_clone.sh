1620a2f1ee116c02bfe7e65968256959e74a8464
Oliver BINNS <mail@oliverbinns.co.uk>
2021-11-25 08:04:17 +0000

    Add develop to refspec

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,9 +3,10 @@ set -e
 
 if [ "$CI_WORKFLOW" = "Pull Request Validation" ]
 then
-  git -C $CI_WORKSPACE status
   git -C $CI_WORKSPACE branch -r
   git -C $CI_WORKSPACE branch temp
+  git -C $CI_WORKSPACE config --replace-all remote.origin.fetch '+refs/develop:refs/remotes/origin/develop'
+  git -C $CI_WORKSPACE branch -r
   git -C $CI_WORKSPACE reset --hard origin/develop
   git -C $CI_WORKSPACE checkout temp
 
