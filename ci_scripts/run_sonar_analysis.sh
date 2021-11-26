e2875ce8e45d75018b18b1b33d4b4848e8e8a70e
Oliver BINNS <mail@oliverbinns.co.uk>
2021-11-26 07:29:38 +0000

    Added Fastlane setup

--- ci_scripts/run_sonar_analysis.sh
+++ ci_scripts/run_sonar_analysis.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
 set -e
 
-fastlane run xcresult_to_junit \
-  xcresult_path:$CI_RESULT_BUNDLE_PATH
-  output_path:"$CI_WORKSPACE/test_output"
-
-fastlane run sonar \
-  project_key:"tube-status-ios" \
-  project_name:"tube-status-ios" \
-  project_version:"1.0" \
-  project_language:"swift" \
-  sonar_runner_args:"-Dsonar.projectBaseDir=$CI_WORKSPACE -Dsonar.c.file.suffixes=- -Dsonar.cpp.file.suffixes=- -Dsonar.objc.file.suffixes=- -Dsonar.pullrequest.provider=github -Dsonar.junit.report_paths=$CI_WORKSPACE/test_output" \
-  sources_path:$CI_WORKSPACE \
-  sonar_organization:"oliver-binns" \
-  sonar_login:$SONAR_TOKEN \
-  sonar_url:"https://sonarcloud.io" \
-  pull_request_branch:"$CI_PULL_REQUEST_SOURCE_BRANCH" \
-  pull_request_base:"$CI_PULL_REQUEST_TARGET_BRANCH" \
-  pull_request_key:$CI_PULL_REQUEST_NUMBER
+cd $CI_WORKSPACE
+fastlane sonar_analysis \
+  result_path:$CI_RESULT_BUNDLE_PATH \
+  workspace:$CI_WORKSPACE \
+  sonar_token:$SONAR_TOKEN \
+  target_branch:$CI_PULL_REQUEST_TARGET_BRANCH \
+  source_branch:$CI_PULL_REQUEST_SOURCE_BRANCH \
+  pr_number:$CI_PULL_REQUEST_NUMBER
