81135fa31b4c12cd8a5b71205c92ded8d63d395e
Oliver BINNS <mail@oliverbinns.co.uk>
2021-12-03 18:37:09 +0000

    Attempt test run as part of build

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -4,5 +4,11 @@ set -e
 if [ "$CI_WORKFLOW" = "Pull Request Validation" ]
 then
   # run in a subshell as this script changes the directory
+  xcodebuild \
+    -workspace TubeStatus.xcworkspace \
+    -scheme "iOS Production" \
+    -destination 'platform=iOS Simulator,name=iPhone 12' \
+    -enable-test-coverage YES \
+    test-without-building
   (./run_sonar_analysis.sh)
 fi
--- ci_scripts/install_dependencies.sh
+++ ci_scripts/install_dependencies.sh
@@ -7,11 +7,6 @@ then
   brew install swiftlint
   brew install sonar-scanner
 
-  echo 'current'
-  ls
-  echo 'workspace'
-  ls $CI_WORKSPACE
-
   (cd $CI_WORKSPACE && fastlane add_plugin xcresult_to_junit)
 elif [ "$CI_WORKFLOW" = "Alpha Release" ]
 then
