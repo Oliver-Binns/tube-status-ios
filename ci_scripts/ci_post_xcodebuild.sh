b6c22f27b87d1f3f93f27427ebd272380554cfe9
Oliver BINNS <mail@oliverbinns.co.uk>
2021-12-03 22:00:12 +0000

    Fixed iOS version

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -7,7 +7,7 @@ then
   xcodebuild \
     -workspace "$CI_WORKSPACE/$CI_XCODE_PROJECT" \
     -scheme "iOS Production" \
-    -destination 'platform=iOS Simulator,name=iPhone 12' \
+    -destination 'platform=iOS Simulator,OS=15.0,name=iPhone 12' \
     -enableCodeCoverage YES \
     test-without-building
   (./run_sonar_analysis.sh)
